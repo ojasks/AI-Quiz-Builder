e748421ab0e66f88cbf3d9ca1b398d9b6c9566af
ojasks <ojaskrsingh24@gmail.com>
2025-07-19 01:35:25 +0530

    Setup Navbar and theme toggle

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["lh3.googleusercontent.com", "avatars.githubusercontent.com"],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  output: "standalone",
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import {prisma} from "@/lib/db"
+import SignInButton from "@/components/SignInButton";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
 
-  export default function Home()
-  {
-    return <Button>HEllo WOrld</Button>
+export default async function Home() {
+  const session = await getServerSession();
+  if (session?.user) {
+    redirect("/dashboard");
   }
-
+  return (
+    <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+      <Card className="w-[300px]">
+        <CardHeader>
+          <CardTitle>Welcome to QuizzGenie 🔥!</CardTitle>
+          <CardDescription>
+            QuizzGenie is a platform for creating quizzes using AI!. Get started by
+            loggin in below!
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <SignInButton text="Sign In with Google" />
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
